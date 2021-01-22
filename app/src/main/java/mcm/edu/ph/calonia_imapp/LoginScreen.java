0ae9eec052b0a416280ff4eb629a2f81a7a9867c
Syncrenize <edermarklaw@gmail.com>
2021-01-23 03:18:29 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/calonia_imapp/LoginScreen.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.calonia_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby_screen);
+    }
+}
---
+++ app/src/main/java/mcm/edu/ph/calonia_imapp/MainScreen.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.calonia_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
